b05bd5e946c4b288d7bd8c400aa16befaf7cfcb7
K926v3C <K926v3C@protonmail.com>
2022-09-11 17:32:36 +0800

    修改文字锚点为正中央，为代码添加注释

--- src/cpst_wp.py
+++ src/cpst_wp.py
@@ -6,6 +6,7 @@ from typing import TYPE_CHECKING
 from PIL import Image, ImageDraw, ImageFont
 
 if TYPE_CHECKING:
+    # 不是很有意义但可以让自动补全很爽的Type Hint
     from datetime import timedelta
     from typing import TypedDict
 
@@ -40,10 +41,14 @@ if TYPE_CHECKING:
         text: Text
 
 
+# 获取当前文件所在文件夹路径
 PATH_PREFIX: str = os.path.split(__file__)[0]
 
 
 def parse_datetime(dt: 'str | DateTimeContainer') -> 'datetime':
+    '''
+    对支持的两种时间表示格式进行解析。
+    '''
     if isinstance(dt, str):
         return datetime.strptime(dt, r'%Y-%m-%d %H:%M:%S')
     else:
@@ -51,13 +56,24 @@ def parse_datetime(dt: 'str | DateTimeContainer') -> 'datetime':
 
 
 def composite(cfg: 'Config', dlt_tm: 'timedelta') -> None:
+    '''
+    合成并保存图片。
+    '''
     with Image.open(os.path.join(PATH_PREFIX, cfg['pic']['rel_folder_path'], cfg['pic']['original_file'])) as img:
+        # 词穷，不会给变量命名，于是全部塞到一句里
         ImageDraw.Draw(img).text(
+            # 参数一：文字坐标
             xy=(img.size[0] * cfg['text']['position']['x'], img.size[1] * cfg['text']['position']['y']),
+            # 参数二：文本内容
             text=cfg['text']['template'] % {'event': cfg['event'], 'day': dlt_tm.days},
+            # 参数三：字体
             font=ImageFont.truetype(cfg['font_path'], size=int(img.size[0] * cfg['text']['size'])),
+            # 参数四：文字锚点
+            anchor='mm',
+            # 参数五：文字颜色（RGB值）
             fill=cfg['text']['color']
         )
+        # 保存图片
         img.save(os.path.join(PATH_PREFIX, cfg['pic']['rel_folder_path'], 'tmp', 'wallpaper.png'))
 
 
@@ -68,6 +84,7 @@ if __name__ == '__main__':
         scheduled_time: 'datetime' = parse_datetime(config['scheduled_time'])
         composite(config, scheduled_time - datetime.today())
     except Exception:
+        # 捕获所有非系统异常，并将信息输出至日志文件。
         import traceback
         with open(os.path.join(PATH_PREFIX, 'log.txt'), encoding='UTF-8') as file:
             file.write(datetime.strftime(datetime.today(), r'%Y-%m-%d %H:%M:%S'))
